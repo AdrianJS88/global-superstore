319509ecb353670288cc7e70cf479717c36c6013
AdrianJS88 <113306625+AdrianJS88@users.noreply.github.com>
2023-07-19 21:06:53 +0300

    final update

--- src/main/java/com/ltp/globalsuperstore/controller/StoreController.java
+++ src/main/java/com/ltp/globalsuperstore/controller/StoreController.java
@@ -1,13 +1,7 @@
 package com.ltp.globalsuperstore.controller;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import javax.validation.Valid;
 
-import com.ltp.globalsuperstore.Constants;
 import com.ltp.globalsuperstore.Item;
 import com.ltp.globalsuperstore.service.StoreService;
 import org.springframework.stereotype.Controller;
@@ -25,27 +19,19 @@ public class StoreController {
 
     @GetMapping("/")
     public String getForm(Model model, @RequestParam(required = false) String id) {
-        model.addAttribute("item",storeService.getItemById(id) );
+
+        model.addAttribute("item",storeService.getItemFromId(id) );
         return "form";
     }
 
     @PostMapping("/submitItem")
-    public String handleSubmit(@Valid Item item, BindingResult result) {
+    public String handleSubmit(@Valid Item item, BindingResult result ,RedirectAttributes redirectAttributes) {
         if (item.getPrice() < item.getDiscount()) {
             result.rejectValue("price", "", "Price cannot be less than discount");
         }
         if (result.hasErrors()) return "form";
-        storeService.submitItem( item );
-//        int index = getIndexFromId(item.getId());
-//        String status = Constants.SUCCESS_STATUS;
-//        if (index == Constants.NOT_FOUND) {
-//            items.add(item);
-//        } else if (within5Days(item.getDate(), items.get(index).getDate())) {
-//            items.set(index, item);
-//        } else {
-//            status = Constants.FAILED_STATUS;
-//        }
-     //   redirectAttributes.addFlashAttribute("status", status);
+        String status = storeService.handleSubmit( item );
+        redirectAttributes.addFlashAttribute("status", status);
         return "redirect:/inventory";
     }
 
@@ -54,19 +40,10 @@ public class StoreController {
         model.addAttribute("items", storeService.getItems());
         return "inventory";
    }
-//
-//    public int getIndexFromId(String id) {
-//        for (int i = 0; i < items.size(); i++) {
-//            if (items.get(i).getId().equals(id)) return i;
-//        }
-//        return Constants.NOT_FOUND;
-//    }
-//
-//    public boolean within5Days(Date newDate, Date oldDate) {
-//        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
-//        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
-//    }
-//
+
+
+
+
 
 
 }
--- src/main/java/com/ltp/globalsuperstore/service/StoreService.java
+++ src/main/java/com/ltp/globalsuperstore/service/StoreService.java
@@ -4,39 +4,57 @@ import com.ltp.globalsuperstore.Constants;
 import com.ltp.globalsuperstore.Item;
 import com.ltp.globalsuperstore.repository.StoreRepository;
 
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class StoreService {
-    StoreRepository  storeRepository = new StoreRepository();
+    StoreRepository storeRepository = new StoreRepository();
 
-public Item getItem(int index){
-    return storeRepository.getItem( index );
-}
+    public Item getItem(int index) {
+        return storeRepository.getItem( index );
+    }
 
-    public void addItem(Item grade){
+    public void addItem(Item grade) {
         storeRepository.addItem( grade );
     }
-    public void  updateItem(Item grade, int index){
-        storeRepository. updateItem( grade,index );
+
+    public void updateItem(Item grade, int index) {
+        storeRepository.updateItem( grade, index );
     }
-    public List<Item> getItems(){
+
+    public List<Item> getItems() {
         return storeRepository.getItems();
     }
+
     public int getItemIndex(String id) {
         for (int i = 0; i < getItems().size(); i++) {
-            if (getItem(i).getId( ).equals(id)) return i;
+            if (getItem( i ).getId().equals( id )) return i;
         }
         return Constants.NOT_FOUND;
-    }public Item getItemById(String id){
-    int index = getItemIndex (id);
-    return  index == Constants.NOT_FOUND ? new Item() : getItem(index);
-}
-    public void  submitItem(Item item){
-        int index = getItemIndex( item.getId() );
-        if (index == Constants.NOT_FOUND) {
-            addItem(item);
-        } else {
-            updateItem(item,index);
-        }
     }
+
+    public Item getItemFromId(String id) {
+        int index = getItemIndex( id );
+        return index == Constants.NOT_FOUND ? new Item() : getItem( index );
+    }
+
+    public boolean within5Days(Date newDate, Date oldDate) {
+        long diff = Math.abs( newDate.getTime() - oldDate.getTime() );
+        return (int) (TimeUnit.MILLISECONDS.toDays( diff )) <= 5;
+    }
+
+    public String handleSubmit(Item item) {
+        int index = getItemIndex( item.getId() );
+        String status = Constants.SUCCESS_STATUS;
+        if (index == Constants.NOT_FOUND) {
+            addItem( item );
+        } else if (within5Days( item.getDate(), getItem( index ).getDate() )) {
+            updateItem( item, index );
+        } else {
+            status = Constants.FAILED_STATUS;
+        }
+        return status;
+    }
+
 }
